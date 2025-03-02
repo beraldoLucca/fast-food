e72a1ee9aa685437f5d4693a5cce1c2b4bbc2258
beraldoLucca <lucca.beraldo@aluno.ifsp.edu.br>
2025-03-02 16:13:59 -0300

    added secret manager implementation

---
+++ src/test/java/br/com/fiap/fast_food/src/configurations/SecretManagerIntegrationTest.java
@@ -0,0 +1,26 @@
+package br.com.fiap.fast_food.src.configurations;
+
+import br.com.fiap.fast_food.src.configurations.aws.AwsSecretsManagerService;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class SecretManagerIntegrationTest {
+
+    @Autowired
+    private AwsSecretsManagerService secretsManagerService;
+
+    @Test
+    public void testGetSecretFromLocalStack() {
+        String secretName = "rds-credentials";
+        JSONObject secret = secretsManagerService.getSecret(secretName);
+
+        assertNotNull(secret, "O segredo não foi recuperado!");
+        System.out.println("✅ Segredo recuperado: " + secret.toString());
+    }
+}
+
